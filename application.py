d179f5375ad6dd7c6b4225981726385d4e253e9e
Syed Moiz <moizeali@gmail.com>
2024-09-16 03:19:27 +0530

    Deployment Configuration

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+
 from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
@@ -10,6 +11,7 @@ application=Flask(__name__)
 app=application
 
 ## Route for a home page
+
 @app.route('/')
 def index():
     return render_template('index.html') 
@@ -19,9 +21,8 @@ def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
     else:
-        try:
-            data=CustomData(
-                gender=request.form.get('gender'),
+        data=CustomData(
+            gender=request.form.get('gender'),
             race_ethnicity=request.form.get('ethnicity'),
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
@@ -29,16 +30,16 @@ def predict_datapoint():
             reading_score=float(request.form.get('writing_score')),
             writing_score=float(request.form.get('reading_score'))
              )
-            pred_df=data.get_data_as_data_frame()
-            print(pred_df)
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
         #print("Before Prediction")
 
-            predict_pipeline=PredictPipeline()
-            #print("Mid Prediction")
-            results=predict_pipeline.predict(pred_df)
-            #print("after Prediction")
-            return render_template('home.html',results=results[0])
-        except Exception as e:
-            # Handle the error and return a friendly message
-            return render_template('home.html', error=str(e))
+        predict_pipeline=PredictPipeline()
+        #print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        #print("after Prediction")
+        return render_template('home.html',results=results[0])
     
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
